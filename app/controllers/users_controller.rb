515bbeebbb2d2bdd56baf8850c6bea511c19dea8
munehiro.kozawa <z9l2c26@gmail.com>
2020-12-25 18:01:56 +0900

    first

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.destroy(params[:id])
+    redirect_to new_user_path
+  end
+end
